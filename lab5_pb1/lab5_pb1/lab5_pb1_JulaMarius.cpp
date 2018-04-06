9586a72a0959da4f5a679e5cc4123f9f33950ae8
julanu <marius.jula@yahoo.com>
2018-04-06 03:07:15 +0300

    up

--- lab5_pb1/lab5_pb1/lab5_pb1_JulaMarius.cpp
+++ lab5_pb1/lab5_pb1/lab5_pb1_JulaMarius.cpp
@@ -16,14 +16,14 @@ refferences to those objects.
 
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+#include <iostream>
 #include <conio.h>
 
-struct Circle {
+using namespace std;
+
+struct CircleStr {
 	int rad;
 };
-double Perimeter(struct Circle *);
-double Area(struct Circle *);
 
 class Circle {
 private:
@@ -32,38 +32,73 @@ public:
 	double getArea(void);
 	double getPerimeter(void);
 	void setVal(int);
+	int getRadius(void);
+	~Circle(void);
+	Circle(int x = 1) {//explicit constructor w/ 1 implicit parameter
+		rad = x;
+	}
 };
+
+//prototypes using the struct
+double Perimeter(struct CircleStr *, int);
+double Area(struct CircleStr *, int);
+
+
+int main()
+{
+	Circle c1, c2;
+	struct CircleStr c[2] = {10, 2};
+	cout << "Struct calling of functions" << endl << "Area" << endl;
+	cout << Area(c, 0) << "\t" << Area(c, 1) << endl;
+	cout << "Perimeter" << endl;
+	cout << Perimeter(c, 0) << "\t" << Perimeter(c, 1) << endl;
+	cout << "Class calling of functions" << endl;
+	c1.setVal(3);//we set the values for our objects
+	c2.setVal(7);
+	cout << "The radius fetched by an accessor has the value of " << c1.getRadius() << endl;
+	cout << "Area" << endl;
+	cout << c1.getArea() << "\t" << c2.getArea() << endl;
+	cout << "Perimeter" << endl;
+	cout << c1.getPerimeter() << "\t" << c2.getPerimeter() << endl;
+	c1.~Circle(); //call of the destructor
+	_getch();
+}
+
+//functions for the struct CircleStr
+//compute the area of a circle
+double Area(struct CircleStr *c, int i)
+{
+	return (3.14 * c[i].rad * c[i].rad);
+}
+//compute the perimeter of a circle
+double Perimeter(struct CircleStr *c, int i)
+{
+	return (2 * 3.14 * c[i].rad);
+}
+
+//functions for the class Circle
+//
 void Circle::setVal(int init_rad)
 {
 	rad = init_rad;
 }
+//compute the area of a circle
 double Circle::getArea(void)
 {
-	return (3. 14 * rad * rad);
+	return (3.14 * rad * rad);
 }
+//compute the perimeter of a circle
 double Circle::getPerimeter(void)
 {
 	return (2 * 3.14 * rad);
 }
-
-
-
-void main()
+//accessor
+int Circle::getRadius(void)
 {
-	/*Circle c1, c2;
-	c1.setVal(3);
-	printf("\n%.2lf",c1.getArea());*/
-
-	//to call functions
-	_getch();
+	return rad;
 }
-
-double Area(struct Circle *c)
+//destructor
+Circle::~Circle(void)
 {
-	return (3.14 * c.rad * c.rad);
-}
-
-double Perimeter(struct Circle *c)
-{
-	return (2 * 3.14 * c.rad);
+	rad = 0;
 }
