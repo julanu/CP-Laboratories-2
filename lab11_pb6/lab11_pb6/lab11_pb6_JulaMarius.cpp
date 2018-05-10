24d51e19ded5f05c797d1560952c1bcd7c612610
julanu <marius.jula@yahoo.com>
2018-05-10 15:05:34 +0300

    up

--- lab11_pb6/lab11_pb6/lab11_pb6_JulaMarius.cpp
+++ lab11_pb6/lab11_pb6/lab11_pb6_JulaMarius.cpp
@@ -10,20 +10,41 @@ keyboard. Examine the error given by the attempt of instantiating the base class
 #define _CRT_SECURE_NO_WARNINGS
 #define MAX 100
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class myString {
-	virtual char * ft_strcat(char [], char []);
-	virtual void ft_interlace();
-	virtual char * ft_reverse(char []);
+	int x;
+public:
+	virtual void test() = 0;
+	virtual char * ft_strcat(char tab[], char tab2[]) { 
+		return tab;
+	}
+//	virtual void ft_interlace();
+//	virtual char * ft_reverse(char []);
 };
 
-class Derivated : public myString {
+class Derived : public myString {
+	int y;
 public:
+	void test() { cout << "test\n"; }
+
 	char * ft_strcat(char tab[], char tab2[]) {
-		char cat[MAX];
-		strcpy(tab, tab2);
-		strcat(cat, tab);
-		return cat;
+		strcat(tab, tab2);
+		return tab;
 	}
 };
+
+int main()
+{
+	Derived ob;
+	ob.test();
+	char tab1[MAX], tab2[MAX];
+	strcpy(tab1, "da"); strcpy(tab2, "nu");
+	cout << ob.ft_strcat(tab1, tab2);
+	cin.get();
+	cin.ignore();
+
+	return 0;
+}
+
